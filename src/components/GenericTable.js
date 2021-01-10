e4cac27e97cbec65245df7e28674f5216d71829b
unknown <ahorn508@gmail.com>
2021-01-10 10:51:26 -0500

    fixed display type on render

--- src/components/GenericTable.js
+++ src/components/GenericTable.js
@@ -1,8 +1,13 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import "../styles/Tables.css"
 import "../styles/generic.css"
 
 function GenericTable(props) {
+  const [mode, setMode] = useState('')
+  useEffect(() => {
+    setMode(props.mode)
+  }, [props.body])
+  
   function generateRows(acl) {
     return (
       <tbody className="tablebody">
@@ -14,7 +19,7 @@ function GenericTable(props) {
               (typeof action.value) === "number" ?
                 (props.headerL === 'Results' 
                 && ['Max Damage', 'DPS'].some(s => action.id.includes(s))
-                && ['effmod', 'puremod'].some(s => props.mode.includes(s))) ?
+                && ['effmod', 'puremod'].some(s => mode.includes(s))) ?
                   <td className="rightrow">{`${(action.value * 100).toPrecision(4) / 1}%`}</td> :
                   <td className="rightrow">{action.value.toPrecision(4) / 1}</td> :
                   <td className="rightrow">{action.value}</td>
